e83e85ee0326e79320bc82278b7d70c9c50c2936
Dillon Kerr <dillon.p.kerr@gmail.com>
2022-05-10 19:46:48 -0400

    Updates (#7)
    Updates 2022-05-10
    
    - Use zsh instead of bash
    - Use homebrew to install any dependencies

--- setup.sh
+++ setup.sh
@@ -1,24 +1,18 @@
-# Clone the pyenv directory into the home directory
-if [ ! -d "$HOME/.pyenv" ]; then
-    git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
-fi
-
-# @TODO: can we always get the latest version?
-# Install NVM
-if [ ! -d "$HOME/.nvm" ]; then
-    sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-fi
+# Install any dependencies
+brew install pyenv nvm
 
 # Bash Script to create symlinks for file in current directory
 # in the current user's home directory
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-for file in .{aliases,bash_profile,bashrc,functions,git-completion,gitignore_global,profile,vimrc}; do
+for file in .{zshrc,aliases,functions,gitignore_global,vimrc}; do
   rm -f $HOME/$file
   ln -s $DIR/$file $HOME/$file
 done
 
 git config --global core.excludesfile $HOME/.gitignore_global
 
+echo ""
+echo "#################"
 echo "Symlinks created!"
+echo "#################"
