057071c490df89d91b62f6f6354d0d9546de9906
Dillon Kerr <dillon.kerr@pillpack.com>
2022-06-20 12:29:46 -0400

    Tweak setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 # Install any dependencies
-brew install pyenv nvm
+brew install nvm
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install solarized Vim theme
+git clone https://github.com/altercation/vim-colors-solarized.git
+mkdir -p ~/.vim/colors
+cp vim-colors-solarized/colors/solarized.vim ~/.vim/colors
+rm -rf vim-colors-solarized/
 
 # Bash Script to create symlinks for file in current directory
 # in the current user's home directory
